4db9a3338636d580cc0ddc7a50ab84f4c54c6c4a
Ana <aparadis.creative@gmail.com>
2026-04-13 16:18:12 -0400

    .next build files removal

--- .next/dev/static/chunks/src_app_page_tsx_0f2un.8._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_027hm3f._.css","static/chunks/src_components_ProductSpinner_tsx_0r~~w9b._.js"],
-    source: "dynamic"
-});
--- .next/server/app/_global-error.segments/_tree.segment.rsc
+++
@@ -1,1 +0,0 @@
-0:{"tree":{"name":"","param":null,"prefetchHints":0,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":0,"slots":null}}},"staleTime":300,"buildId":"05HLfpf6Wm2Q9OnMfvd8t"}
